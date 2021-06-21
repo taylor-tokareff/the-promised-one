e4a6e981cf95411f02d5e4b59411d5cb45d93b80
taylor-tokareff <taylortokareff@gmail.com>
2021-06-20 22:50:28 -0700

    biscuits post test fails as expected

---
+++ __tests__/app-biscuits.test.js
@@ -0,0 +1,27 @@
+import pool from '../lib/utils/pool';
+import setup from '../data/setup';
+import request from 'supertest';
+import app from '../lib/app';
+import Biscuit from '../lib/models/Biscuit';
+
+describe('demo routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  test('creates a biscuit via POST', async () => {
+
+    const res = await request(app)
+      .post('/api/v1/biscuits')
+      .send({
+        flour: 'white',
+        sauce: 'butter',
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      flour: 'white',
+      sauce: 'butter',
+    });
+
+  });
