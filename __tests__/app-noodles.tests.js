d8dcd5c835a2d761482ff937ccd23806dd771e68
taylor-tokareff <taylortokareff@gmail.com>
2021-06-21 09:45:26 -0700

    find all noodles fails as expected

--- __tests__/app-noodles.tests.js
+++ __tests__/app-noodles.tests.js
@@ -37,6 +37,24 @@ describe('demo routes', () => {
   });
 
 
+  test('finds all noodles via GET route', async () => {
+
+    const noodle1 = await Noodle.insert({
+      name: 'spaghetti',
+      region: 'italy'
+    });
+    const noodle2 = await Noodle.insert({
+      name: 'pad thai',
+      region: 'thailand'
+    });
+    const noodle3 = await Noodle.insert({
+      name: 'soba',
+      region: 'japan'
+    });
+
+    const res = await request(app).get('/api/v1/noodles');
+    expect(res.body).toEqual([noodle1, noodle2, noodle3]);
+  });
 
 
 
